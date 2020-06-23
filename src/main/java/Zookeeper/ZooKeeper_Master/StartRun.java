b690a5df58425f2eb2347e532cc68f7c5fb6a552
mcl <48353260+mcl973@users.noreply.github.com>
2020-06-23 20:00:47 +0800

    Create StartRun.java

---
+++ src/main/java/Zookeeper/ZooKeeper_Master/StartRun.java
@@ -0,0 +1,12 @@
+package Zookeeper.ZooKeeper_Master;
+
+import java.io.IOException;
+
+/**
+ * 测试使用
+ */
+public class StartRun {
+    public static void main(String[] args) throws IOException {
+        new ContainerAndHandler();
+    }
+}
